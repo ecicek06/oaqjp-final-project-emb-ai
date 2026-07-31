a91d1117e766f8bf3057e88dc73af5ece9c48881
ecicek06 <59051362+ecicek06@users.noreply.github.com>
2026-07-31 03:21:17 +0300

    Add files via upload

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,16 +2,37 @@ import json
 import requests
 
 def emotion_detector(text_to_analyze):
+
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=header)
-    formatted_text = json.loads(response.text)
+    
+    if response.status_code == 200:
+        formatted_text = json.loads(response.text)
+        emotions_dict = formatted_text["emotionPredictions"][0]["emotion"]
+        max_score = max(emotions_dict.values())
+        dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+        emotions_dict.update({"dominant_emotion" : dominant_emotion})
 
-    emotions_dict = formatted_text["emotionPredictions"][0]["emotion"]
-    max_score = max(emotions_dict.values())
-    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
-    emotions_dict.update({"dominant_emotion" : dominant_emotion})
+    elif response.status_code == 400:
+        emotions_dict = {
+            "anger" : None, 
+            "disgust" : None, 
+            "fear" : None, 
+            "joy" : None, 
+            "sadness" : None, 
+            "dominant_emotion" : None 
+        }    
+    else:
+        emotions_dict = {
+        "anger" : None, 
+        "disgust" : None, 
+        "fear" : None, 
+        "joy" : None, 
+        "sadness" : None, 
+        "dominant_emotion" : None
+        }
 
-    return emotions_dict
+    return emotions_dict            
     
