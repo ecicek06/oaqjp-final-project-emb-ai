9f117648f943862d560466d4f3d5b5d59db5a350
ecicek06 <59051362+ecicek06@users.noreply.github.com>
2026-07-31 03:21:55 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+'''
+Import required libraries
+'''
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/emotionDetector')
+def sent_emotion():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detection over it using emotion_detector()
+        function. The output returned shows emotions and confidence 
+        scores for the provided text.
+    '''
+    text_to_analyze = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyze)
+
+    anger = response["anger"]
+    disgust = response["disgust"]
+    fear = response["fear"]
+    joy = response["joy"]
+    sadness = response["sadness"]
+    dominant = response["dominant_emotion"]
+
+    if dominant is None:
+        return "Invalid text! Please try again!"
+
+    return (f"For the given statement, the system response is 'anger' : {anger},"
+            f" 'disgust' : {disgust}, 'fear' : {fear}, 'joy' : {joy}, 'sadness' : {sadness}."
+            f" The dominant emotion is {dominant}.")
+
+
+@app.route('/')
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run("localhost", port=5001)
